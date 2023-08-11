f5724e8b7c10e2718d8a35bf80ae2281ca06e54e
Paulo Henrique Silva Matias <140565781+phsmatias@users.noreply.github.com>
2023-08-11 16:33:20 -0300

    Create f_math.h

---
+++ funções1/ex2/f_math.h
@@ -0,0 +1,10 @@
+//Função que informe se o valor é positivo
+int delta();
+
+// Função para exibir os dados na tela
+void exibir_dados(int x);
+
+//Função para leitura de dados
+int ler_dados1(void);
+int ler_dados2(void);
+int ler_dados3(void);
