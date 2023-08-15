bf14d16a214b852b98a36b2d2db3d27a4b18d15c
Paulo Henrique Silva Matias <140565781+phsmatias@users.noreply.github.com>
2023-08-15 16:55:38 -0300

    Create main.c

---
+++ funções1/pirâmide/main.c
@@ -0,0 +1,14 @@
+/*ENUNCIADO 3. Faça o programa que apresenta a seguinte saída, perguntando ao usuário o número máximo (no exemplo, 9). Este número deve ser sempre ímpar.
+1 2 3 4 5 6 7 8 9
+2 3 4 5 6 7 8
+3 4 5 6 7
+4 5 6
+5 */
+
+
+#include "funcoes.h"
+#include <stdio.h>
+
+int main(void){
+    exibir_piramide(tamanho());
+}
