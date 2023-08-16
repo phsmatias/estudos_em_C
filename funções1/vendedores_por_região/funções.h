3822ad6d75e9359101c2038a0a058301ee21d886
Paulo Henrique Silva Matias <140565781+phsmatias@users.noreply.github.com>
2023-08-16 17:38:41 -0300

    Create funções.h

---
+++ funções1/vendedores_por_região/funções.h
@@ -0,0 +1,8 @@
+//Função que lê um caractere
+char ler_caractere(void);
+
+//Função que vai relalizar os condicionais
+void switch_function(char a);
+
+//Função para realizar a limpeza do Buffer
+void limparBuffer(void);
