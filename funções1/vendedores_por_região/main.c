c53e6203e835a274365056122e7b72958297a2fb
Paulo Henrique Silva Matias <140565781+phsmatias@users.noreply.github.com>
2023-08-16 17:24:35 -0300

    Create main.c

---
+++ funções1/vendedores_por_região/main.c
@@ -0,0 +1,16 @@
+/*
+    Sistema de banco de dados simples
+
+    Seleciona vendedores por região
+*/
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "funcoes.h"
+
+int main(void){
+    char c;
+    c = ler_caractere();
+    switch_function(c);
+    return 0;
+}
